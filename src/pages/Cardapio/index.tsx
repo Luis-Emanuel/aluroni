1bd55cff549b0e08ccaf8906babbb1cfb21b3172
L_Emanuel <43679743+Luis-Emanuel@users.noreply.github.com>
2022-08-02 18:41:50 -0300

    Criamos o header
    -Utilização de variáveis
    -Normalize do npm
    -absolute imports

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,11 +1,16 @@
 import styles from "./Cardapio.module.scss"
-import {ReactComponent as Logo} from "../../assets/logo.svg"
+import {ReactComponent as Logo} from "assets/logo.svg"
 export default function Cardapio(){
     return(
         <main>
             <nav className={styles.menu}>
-            <Logo/>
+            <Logo/> 
             </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    A casa do código e da massa
+                </div>
+            </header>
         </main>
     )
 }
