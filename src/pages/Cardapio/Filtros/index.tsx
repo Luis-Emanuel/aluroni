0b1a07d6fce1c7ee340199b8e84289317dad4c78
L_Emanuel <43679743+Luis-Emanuel@users.noreply.github.com>
2022-08-22 14:14:34 -0300

    Rotas

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -1,6 +1,6 @@
-import filtros from "./filtros.json"
-import styles from "./Filtros.module.scss"
-import classNames from "classnames"
+import filtros from './filtros.json';
+import styles from './Filtros.module.scss';
+import classNames from 'classnames';
 type IOpcao = typeof filtros[0]
 
 interface Props{
@@ -8,24 +8,24 @@ interface Props{
     setFiltro: React.Dispatch<React.SetStateAction<number | null>>
 }
 export default function Filtros({filtro, setFiltro}: Props){
-    function selecionarFiltro(opcao: IOpcao){
-        if(filtro === opcao.id) return setFiltro(null);
-        return setFiltro(opcao.id)
-    }
-    return(
-        <div className={styles.filtros}>
-            {filtros.map((opcao) =>(
-                <button 
-                 className={classNames({
-                    [styles.filtros__filtro]: true,
-                    [styles["filtros__filtro--ativo"]]: filtro === opcao.id
-                 })} 
-                 key={opcao.id} 
-                 onClick={() => selecionarFiltro(opcao)}
-                >
-                    {opcao.label}
-                </button>
-            ))}
-        </div>
-    )
+  function selecionarFiltro(opcao: IOpcao){
+    if(filtro === opcao.id) return setFiltro(null);
+    return setFiltro(opcao.id);
+  }
+  return(
+    <div className={styles.filtros}>
+      {filtros.map((opcao) =>(
+        <button 
+          className={classNames({
+            [styles.filtros__filtro]: true,
+            [styles['filtros__filtro--ativo']]: filtro === opcao.id
+          })} 
+          key={opcao.id} 
+          onClick={() => selecionarFiltro(opcao)}
+        >
+          {opcao.label}
+        </button>
+      ))}
+    </div>
+  );
 }
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,38 +1,27 @@
-import styles from "./Cardapio.module.scss"
-import {ReactComponent as Logo} from "assets/logo.svg"
-import Buscador from "./Buscador"
-import { useState } from "react"
-import { serialize } from "v8";
-import Filtros from "./Filtros";
-import Ordenador, { OpcoesOrdenador } from "./Ordenador";
-import Itens from "./itens";
+import styles from './Cardapio.module.scss';
+import Buscador from './Buscador';
+import { useState } from 'react';
+import Filtros from './Filtros';
+import Ordenador, { OpcoesOrdenador } from './Ordenador';
+import Itens from './itens';
+import Menu from 'components/Menu';
 
 export default function Cardapio(){
-    const [busca,setBusca]= useState("");
-    const [filtro, setFiltro]= useState<number | null>(null);
-    const [ordenador, setOrdenador] = useState<OpcoesOrdenador>("");
-    return(
-        <main>
-            <nav className={styles.menu}>
-            <Logo/> 
-            </nav>
-            <header className={styles.header}>
-                <div className={styles.header__text}>
-                    A casa do código e da massa
-                </div>
-            </header>
-            <section className={styles.cardapio}>
-                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-                <Buscador
-                busca={busca}
-                setBusca={setBusca}
-                />
-                <div className={styles.cardapio__filtros}>
-                    <Filtros filtro={filtro} setFiltro={setFiltro}/>
-                    <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
-                </div>
-                <Itens filtro={filtro} busca={busca} ordenador={ordenador}/>
-            </section>
-        </main>
-    )
+  const [busca,setBusca]= useState('');
+  const [filtro, setFiltro]= useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState<OpcoesOrdenador>('');
+  return( 
+    <section className={styles.cardapio}>
+      <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+      <Buscador
+        busca={busca}
+        setBusca={setBusca}
+      />
+      <div className={styles.cardapio__filtros}>
+        <Filtros filtro={filtro} setFiltro={setFiltro}/>
+        <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
+      </div>
+      <Itens filtro={filtro} busca={busca} ordenador={ordenador}/>
+    </section>
+  );
 }
